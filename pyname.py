e4d60b742ec68c2d25c981e7d118a7964241b3ee
Kairatkroy <nicekirt8@gmail.com>
2026-02-06 12:16:27 +0600

    first34

--- pyname.py
+++ pyname.py
@@ -1,7 +1,26 @@
-l = "joi"
+import json
+import yaml
 
-l1 = "jofsd"
+data = {
+        "fruits": ["apple", "banana",'"orange", "grape", "melon"'],
+         "numbers": ["1, 2, 3, 4, 5"],
+        "fds": {"hoi": 21}
+       }
 
-g = "kofpsdf"
 
-sdf = "fdsf"
+
+with open("kloi", "w") as w:
+    coin = yaml.dump(data, w)
+
+with open("kloi") as r:
+    h = yaml.safe_load(r)
+
+
+json_new = json.dumps(data, indent=4)
+
+with open("json1", "w") as w:
+    w.write(json_new)
+
+with open("json1") as r:
+    json1 = json.load(r)
+    print(json1)
