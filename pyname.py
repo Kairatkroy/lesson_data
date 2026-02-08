4119aa8963e168bd8814b25d6ad007b1d32da7d6
Kairatkroy <nicekirt8@gmail.com>
2026-02-08 11:21:46 +0600

    third

--- pyname.py
+++ pyname.py
@@ -1,26 +1,47 @@
 import json
+from xml.etree.ElementTree import indent
+
 import yaml
+import pandas
+
+import pandas as pd
+import numpy as np
 
 data = {
-        "fruits": ["apple", "banana",'"orange", "grape", "melon"'],
-         "numbers": ["1, 2, 3, 4, 5"],
-        "fds": {"hoi": 21}
-       }
+    "ints": [1, 2, 3],
+    "floats": [1.1, 2.2, 3.3],
+    "bools": [True, False, True],
+    "strings": ["яблоко", "приправа", "корнос"],
+    "dates": (["2024-01-01", "2024-01-02", "2024-01-03"]),
+    "categories":(["A", "B", "A"]),
+
+}
+
+
+panda = pd.DataFrame(data,) # превращение файла в pandas файл при "DataFrame"
+panda.to_csv("panda.csv", index=False) # сохранение данных в файл при помощи "to_csv"
+
+js = json.dumps(data, indent=3, ensure_ascii=False) # стерилизация файла в json формат при "dumps"
+with open("json.format", "w", encoding="utf-8") as j: # 1
+    j.write(js) # 2 # сохранение дынных в файл при 1 и 2 "write"
+
+with open("json.format", "r", encoding="utf-8",) as r:
+    re = json.load(r) # стерилизация формата из json на dict при "load"
+    print(re)
+
+with open("js", "w", encoding="utf-8") as w:
+    json.dump(data, w, indent=4, ensure_ascii=False) # сокращение кода при "dump" стерилизация и сохранение "ensure_ascii=False" - чтение кирилицы
 
 
 
-with open("kloi", "w") as w:
-    coin = yaml.dump(data, w)
 
-with open("kloi") as r:
-    h = yaml.safe_load(r)
+with open("yaml.format", "w", encoding="utf-8") as w:
+    yaml.dump(data, w, allow_unicode=True) # стерилизация и сохранение в ямл формат "dump" "allow_unicode=True" - чтение кирилицы
+
+with open("yaml.format", "r", encoding="utf-8") as r:
+    re = yaml.safe_load(r) # стерилизация формата из json на обычный тип данных при "load"
+    print(re)
 
 
-json_new = json.dumps(data, indent=4)
 
-with open("json1", "w") as w:
-    w.write(json_new)
 
-with open("json1") as r:
-    json1 = json.load(r)
-    print(json1)
